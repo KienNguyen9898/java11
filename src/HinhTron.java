1e0acc2ca2ddce75ddf3e7c8873973c8d44849c1
Nguyễn Trọng Kiên <126797297+KienNguyen9898@users.noreply.github.com>
2023-03-06 16:50:33 +0700

    Add files via upload

---
+++ src/HinhTron.java
@@ -0,0 +1,22 @@
+package day3b;
+
+public class HinhTron {
+    double banKinh;
+    public HinhTron(){
+
+    }
+    public HinhTron(double banKinh){
+        this.banKinh = banKinh;
+    }
+    public double tinhChuViHinhTron(){
+        return banKinh * 2 * 3.14;
+    }
+    public double tinhDienTichHinhTron(){ return banKinh * banKinh * 3.14;}
+
+    @Override
+    public String toString() {
+        return "hinhTron{" +
+                "banKinh=" + banKinh +
+                '}';
+    }
+}
